3f31f047ad302bb36713588fa884ba84613346c6
Jabril-A <98670900+Jabril-A@users.noreply.github.com>
2022-03-05 21:03:17 -0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,42 @@
+{
+    "sigleQuote"; true,
+    "arrowParens"; "avoid"
+}
+
+'use strict';
+console.log(doument.querySelector('.message'). 
+textContent);
+//video 70
+document.querySelector('message').textContent ='good job';
+
+//75-77
+document.querySelector('number').textContent ='13';
+document.querySelector('score').textContent ='10';
+
+document.querySelector('guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function() {const guess = Number(document.querySelector('.guess'))})
+ if (!guess) { document.querySelector('.message').textConten = 'no number';} 
+ else if (guess=== secretNumber) { document.querySelector('.message').textConten = 'correct';
+document.querySelector('body').style.backgroundColor = '#60b347'; document.querySelector('.number').style.width='30rem'; 
+}
+ else if (guess > secretNumber) { if (score > 1) {}};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
